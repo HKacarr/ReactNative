caa7c2e01f09218304b2c9b0430dbd1269ed7d30
Hasan Kacar <hasan.kacar@onremote.ch>
2023-09-19 22:48:30 +0300

    SomeSamples revised

--- SomeSamples/index.js
+++ SomeSamples/index.js
@@ -6,6 +6,6 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import GradientView from './src/GradientView';
-import ActionSheet from './src/ActionSheet';
+import ActionSheetComponent from './src/ActionSheet';
 
-AppRegistry.registerComponent(appName, () => ActionSheet);
+AppRegistry.registerComponent(appName, () => ActionSheetComponent);
--- SomeSamples/src/ActionSheet/index.js
+++ SomeSamples/src/ActionSheet/index.js
@@ -1,28 +1,51 @@
 import React, {Component} from 'react';
 import {SafeAreaView, Text, TouchableOpacity} from 'react-native';
-
-let _openActionSheet = () => {
-    this.ActionSheet.show();
-}
+import {ActionSheetCustom as ActionSheet} from 'react-native-actionsheet';
 
 
-const ActionSheet = () => {
+const ActionSheetComponent = () => {
     return (
         <SafeAreaView>
-            <TouchableOpacity onPress={_openActionSheet}>
+            <TouchableOpacity onPress={() => this.ActionSheet.show()}>
                 <Text>Open to Actionsheet</Text>
             </TouchableOpacity>
 
             <ActionSheet
                 ref={ref => this.ActionSheet = ref}
-                title={"Whic you want to select from bottom selection!"}
-                options={["New Product", "Edit Product", "Delete", "Cancel"]}
+                title={<Text style={{color: '#000', fontSize: 18, fontWeight: "700"}}>Which one do you like?</Text>}
+                options={[
+                    <Text style={{color: 'green', fontSize: 18, fontWeight: "600"}}>Add Product</Text>,
+                    <Text style={{color: 'blue', fontSize: 18, fontWeight: "600"}}>Edit Product</Text>,
+                    <Text style={{color: 'red', fontSize: 18, fontWeight: "600"}}>Delete</Text>,
+                    "Cancel"
+                ]}
+                cancelButtonIndex={3}
+                destructiveButtonIndex={2}
                 onPress={(index) => {
+                    switch (index) {
+                        case 0:
+                            alert("Add Process Selected from ActionSheet");
+                            break;
 
+                        case 1:
+                            alert("Edit Process Selected from ActionSheet");
+                            break;
+
+                        case 2:
+                            alert("Remove Process Selected from ActionSheet");
+                            break;
+
+                        case 3:
+                            alert("Cancel Process Selected from ActionSheet");
+                            break;
+
+                        default:
+                            alert("Error!");
+                    }
                 }}
             />
         </SafeAreaView>
     )
 }
 
-export default ActionSheet;
+export default ActionSheetComponent;
